454e3362140fbbb7a0c256eab96c51fbfe52d822
mueller_tsai <mueller_tsai@walsin.com>
2023-08-04 22:19:13 +0800

    8.4. update from office

--- src/main/java/com/mt/app/model/dao/LeaveDAO.java
+++
@@ -1,8 +0,0 @@
-package com.mt.app.model.dao;
-
-import com.mt.app.model.entity.Leave;
-import com.mt.core.model.dao.BaseJpaRepository;
-
-public interface LeaveDAO extends BaseJpaRepository<Leave,Integer> {
-    
-}
